b7be130c18de30a9332c51aec76ed485497358a3
Mario Barceinas <dev.mariobarceinas@gmail.com>
2018-08-22 20:42:17 -0500

    Cahage user strict to use strict xD

--- order-buttons/main.js
+++ order-buttons/main.js
@@ -1,5 +1,5 @@
 (function() {
-    'user strict';
+    'use strict';
 
     const newButton = document.querySelector('#generateButtons');
     const arrNumbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
