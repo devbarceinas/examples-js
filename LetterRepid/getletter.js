dc419703ed2696a708e61542e37cfa6df20cfeeb
Mario Joaquin <mariojoaquin.lopez.contractor.next@bbva.com>
2018-12-03 09:48:41 -0600

    a hora es una función

--- LetterRepid/getletter.js
+++ LetterRepid/getletter.js
@@ -2,10 +2,12 @@
 
   'use strict';
 
-  let saludo = 'asdfghjkfkkjjj';
-  let splitSaludo = saludo.split('');
-  let repetido = splitSaludo.sort((x,y) => splitSaludo.filter(e => e === x).length - splitSaludo.filter(e => e === y).length).pop();
+  const searchLetter = (saludo) => {
+    let splitSaludo = saludo.split('');
+    let repetido = splitSaludo.sort((x, y) => splitSaludo.filter(e => e === x).length - splitSaludo.filter(e => e === y).length).pop();
+    return repetido;
+  }
 
-  console.log(repetido);
+  console.log(searchLetter('asdfghjkfkkjjj'));
 
 })();
